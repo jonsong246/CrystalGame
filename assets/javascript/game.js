b8c4a363e63f120f4acb3e9ab6067c3d4c55ac64
Frostevia <jonsong246@gmail.com>
2018-08-23 18:29:38 -0700

    fix

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,12 +4,13 @@ $('.total').text(total)
 $('.goal').text(goal)
 
 for (let i = 1; i < 4; i++) {
-    $('.crystalRow').append('<div class="col-md-4"><img class="crystal" id="crystal${i}" data-crystal=${Math.floor*(Math.random() * 20) + 1} src="assets/images/crystal${i}.png" alt="crystal${i}"></div>'
-)}
+    $('.crystalRow').append(`<div class="col-md-4"><img class="crystal" id="crystal${i}" data-crystal=${Math.floor(Math.random() * 20) + 1} src="assets/images/crystal${i}.png" alt="crystal${i}"></div>`)
+}
 
 
-$('.crystal').on('click', function(){
+$(document).on('click', ".crystal", function(){
     let crystalValue = $(this).attr('data-crystal')
+    console.log(crystalValue);
     total += parseInt(crystalValue)
     $('.total').text(total)
     gameStatus();
